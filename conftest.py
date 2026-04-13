e7ab68f4967992fd015c0f5a94a1e7407793b0cc
ovcarj <juraj.ovcar@gmail.com>
2026-04-13 07:59:26 +0200

    ROOT: Initialize project using aiida-plugin-cutter
    For the cookie cutter recipe, see:
    https://github.com/aiidateam/aiida-plugin-cutter

---
+++ conftest.py
@@ -0,0 +1,16 @@
+"""pytest fixtures for simplified testing."""
+
+import pytest
+
+pytest_plugins = ["aiida.manage.tests.pytest_fixtures"]
+
+
+@pytest.fixture(scope="function", autouse=True)
+def clear_database_auto(clear_database):  # pylint: disable=unused-argument
+    """Automatically clear database in between tests."""
+
+
+@pytest.fixture(scope="function")
+def charmm_gui_code(aiida_local_code_factory):
+    """Get a charmm_gui code."""
+    return aiida_local_code_factory(executable="diff", entry_point="charmm_gui")
---
+++ src/aiida_charmm_gui/__init__.py
@@ -0,0 +1,7 @@
+"""
+aiida_charmm_gui
+
+AiiDA plugin for using the CHARMM-GUI REST-like API.
+"""
+
+__version__ = "0.1.0a0"
